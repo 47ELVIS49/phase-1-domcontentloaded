583b71205c1a10d33cb97e268c739917a34aaa43
Elvis Momanyi <elvismomanyi23@gmail.com>
2024-01-15 06:24:24 +0300

    done with the lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    // Update the text content here
+    document.querySelector('p').textContent = 'This is really cool!';
+  });
+  
